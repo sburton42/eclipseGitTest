50fae689fc64e7343e3d11354f0162b16e9a7a67
Scott Burton <scotthburton@gmail.com>
2014-07-07 10:00:39 -0600

    local

--- eclipseGitProject/src/eclipseGitProject/HelloWorld.java
+++ eclipseGitProject/src/eclipseGitProject/HelloWorld.java
@@ -5,9 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int n5;
-		
-		
+		int n10;
+				
 		System.out.println("Hello World");
 		
 		for (int i = 0; i < 10; i++) {
