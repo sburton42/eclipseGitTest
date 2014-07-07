44c1b77643562b9183032942202bdb751362ec17
Scott Burton <scotthburton@gmail.com>
2014-07-07 09:38:53 -0600

    local change

--- eclipseGitProject/src/eclipseGitProject/HelloWorld.java
+++ eclipseGitProject/src/eclipseGitProject/HelloWorld.java
@@ -4,7 +4,14 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int n1;
+		
 		System.out.println("Hello World");
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
